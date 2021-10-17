4cb33ec419235ee4fb327b9800672ad998b3c050
Rameshwar Kshirsagar <kshirsagarram253@gmail.com>
2021-10-17 17:22:46 +0530

    Initial commit

---
+++ src/components/users/AddUser.js
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+import axios from 'axios'
+import { useHistory } from "react-router-dom";
+
+const AddUser = () => {
+  let history = useHistory();
+  const [user, setUser] = useState({
+    name: "",
+    Address: "",
+    email: "",  
+    phone: "",
+    Age: "",
+    img:""
+  });
+
+  const { name, Address, email, phone, Age,img } = user;
+  const onInputChange = e => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    await axios.post("http://localhost:3003/users", user);
+    history.push("/");
+  };
+
+  return (
+    <div className="container">
+      <div className="w-50 mx-auto shadow p-5">
+        <h2 className="text-center mb-4">Add A User</h2>
+        <form onSubmit={e => onSubmit(e)}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your Name"
+              name="name"
+              required='true'
+              value={name}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your Address"
+              required='true'
+              name="Address"
+              value={Address}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control form-control-lg"
+              placeholder="Enter Your E-mail"
+              name="email"
+              required="true"
+              value={email}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your Phone Number"
+              name="phone"
+              value={phone}
+              required='true'
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter your Age"
+              name="Age"
+              required='true'
+              value={Age}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          {/* <div className="form-group">
+          <input type="file" id="myFile" name="filename"
+           className="form-control form-control-lg"
+           />
+          </div> */}
+           <div  className="form-group">
+                <input type="file"
+                 value={img}
+                 className="form-control form-control-lg"
+                 type="file"
+                 required='true'
+                 name="img"
+                 onChange={e => onInputChange(e)}   //onChange={e => onInputChange(e.target.file[0])} //
+
+                />
+                
+            </div>
+          <button className="btn btn-primary btn-block">Add User</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddUser;
---
+++ src/components/users/EditUser.js
@@ -0,0 +1,109 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useHistory, useParams } from "react-router-dom";
+
+const EditUser = () => {
+  let history = useHistory();
+  const { id } = useParams();
+  const [user, setUser] = useState({
+    name: "",
+    Address: "",
+    email: "",
+    phone: "",
+    Age: "",
+    img:""
+  });
+
+  const { name, Address, email, phone, Age,img } = user;
+  const onInputChange = e => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  useEffect(() => {
+    loadUser();
+  }, []);
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    await axios.put(`http://localhost:3003/users/${id}`, user);
+    history.push("/");
+  };
+
+  const loadUser = async () => {
+    const result = await axios.get(`http://localhost:3003/users/${id}`);
+    setUser(result.data);
+  };
+  return (
+    <div className="container">
+      <div className="w-75 mx-auto shadow p-5">
+        <h2 className="text-center mb-4">Edit A User</h2>
+        <form onSubmit={e => onSubmit(e)}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your Name"
+              name="name"
+              value={name}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your Address"
+              name="Address"
+              value={Address}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control form-control-lg"
+              placeholder="Enter Your E-mail"
+              name="email"
+              value={email}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your Phone Number"
+              name="phone"
+              value={phone}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your Age "
+              name="Age"
+              value={Age}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          {/* <div  className="form-group">
+                <input type="file"
+                 value={img}
+                 className="form-control form-control-lg"
+                 type="file"
+                //  required='true'
+                 name="img"
+                 onChange={e => onInputChange(e.target.file[0])}
+                />
+                
+            </div> */}
+          <button className="btn btn-warning btn-block">Update User</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default EditUser;
